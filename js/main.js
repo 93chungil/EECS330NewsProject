16a47e3a22728a7cf9bf1bba9340e792acfa15a8
Chung <36549003+93chungil@users.noreply.github.com>
2018-02-19 01:46:00 -0600

    Profiles Framework

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ window.onload = function() {
 	}, 1000)
 }
 
+document.getElementById("defaultOpen").click();
+
 $(document).ready(function() {
 
 	//slide out menu
@@ -34,3 +36,24 @@ $(document).ready(function() {
 	});
 
 });
+
+function openInfo(evt, InfoName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(InfoName).style.display = "block";
+    evt.currentTarget.className += " active";
+}

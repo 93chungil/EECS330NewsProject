de34b9b81c6735ca2866de43d2123ede8f74b6d5
Matt Moderwell <mmoderwell@gmail.com>
2018-02-19 13:55:05 -0600

    Removed jQuery dependency

--- js/main.js
+++ js/main.js
@@ -2,58 +2,53 @@ window.onload = function() {
 	setTimeout(() => {
 		document.getElementById('status').style.display = 'none';
 		document.getElementById('preloader').style.display = 'none';
-	}, 1000)
+	}, 1000);
 }
 
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
 
-$(document).ready(function() {
+function sidebar() {
 
-	//slide out menu
-	$("#sidebar-opener").click(function(e) {
-		e.preventDefault();
-		$('#sidebar-opener').toggleClass('open');
-		$("#wrapper").toggleClass("toggled");
-	});
-	$(document).keyup(function(e) {
-		if (e.keyCode == 27 && $('#sidebar-opener').hasClass('open')) { // escape key maps to keycode `27`
-			$('#sidebar-opener').toggleClass('open');
-			$("#wrapper").toggleClass("toggled");
+	let opener = document.getElementById('sidebar-opener');
+	let wrapper = document.getElementById('wrapper');
+	opener.addEventListener('click', () => {
+		if (opener.getAttribute('class')) {
+			opener.removeAttribute('class', 'open');
+			wrapper.removeAttribute('class', 'toggled');
+		} else {
+			opener.setAttribute('class', 'open');
+			wrapper.setAttribute('class', 'toggled');
 		}
 	});
-
-	$("#places").click(function() {
-		$("#footer").hide();
-		$(".account-page").hide();
-		$("#google-map").show();
-	});
-
-	$("#account").click(function() {
-		$(".account-page").show();
-		$(".footer").show();
-		$("#google-map").hide();
-
-	});
-
-});
+	document.onkeydown = function(e) {
+		e = e || window.event;
+		if (e.keyCode == 27) {
+			if (opener.getAttribute('class')) {
+				opener.removeAttribute('class', 'open');
+				wrapper.removeAttribute('class', 'toggled');
+			}
+		}
+	};
+};
+sidebar();
 
 function openInfo(evt, InfoName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+	// Declare all variables
+	var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(InfoName).style.display = "block";
-    evt.currentTarget.className += " active";
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(InfoName).style.display = "block";
+	evt.currentTarget.className += " active";
 }

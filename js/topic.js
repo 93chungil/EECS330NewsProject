d4329f3445e2b494fcfc95aa765b8058ff73b3e8
Chung <36549003+93chungil@users.noreply.github.com>
2018-03-04 02:59:08 -0600

    Thumbs up, Thumbs down and sorting now works.
    Sorts by having the comment with highest score (likes - dislikes) at the top, and the rest by most recent

--- js/topic.js
+++ js/topic.js
@@ -60,34 +60,149 @@ get_articles();
 var savedcomments = localStorage.getItem(curr_topic+"comments");
 let comments_array;
 if (savedcomments == null) {
-	comments_array = [{userID: "chungLee", comment: "I think this website is awesome!", datetime: "Sat Mar 03 2018 21:07:02", likes: 100, dislikes: 1}];
+	comments_array = [{userID: "chungLee", comment: "I think this website is awesome!", datetime: "Sat Mar 03 2018 21:07:02", likes: 0, dislikes: 0}];
 }
 else {
 	comments_array = JSON.parse(savedcomments);
 }
 
+function increase_like(ele) {
+	var comment = ele.id;
+	comment = comment.split(";|,,")[0];
+	for(var i=0; i<comments_array.length; i++) {
+		if (comments_array[i]["comment"] == comment){
+			comments_array[i]["likes"]++; 
+			var numberSpan = document.getElementById(comment+";|,,like");
+			numberSpan.innerHTML = parseInt(comments_array[i]["likes"]);
+			break;
+		}
+	}
+
+	sort_comments();
+}
+
+function increase_dislike(ele) {
+	var comment = ele.id;
+	comment = comment.split(";|,,")[0];
+	for(var i=0; i<comments_array.length; i++) {
+		if (comments_array[i]["comment"] == comment){
+			comments_array[i]["dislikes"]++; 
+			var numberSpan = document.getElementById(comment+";|,,dislike");
+			numberSpan.innerHTML = parseInt(comments_array[i]["dislikes"]);
+			break;
+		}
+	}
+
+	sort_comments();
+}
+
+function arraymove(arr, fromIndex, toIndex) {
+    var element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+}
+
+function sort_comments() {
+	var topscore = -999999;
+	var topindex;
+	for(var i=0;i<comments_array.length; i++) {
+		var score = comments_array[i]["likes"]-comments_array[i]["dislikes"];
+		if (score > topscore) {
+			topindex = i;
+			topscore = score;
+		}
+	}
+	var ul = document.getElementById('commentListID');
+  	if (ul) {
+	    while (ul.firstChild) {
+	      ul.removeChild(ul.firstChild);
+	    }
+	}
+	arraymove(comments_array, topindex, 0);
+	initialize_comments();
+	savedcomments = JSON.stringify(comments_array);
+	localStorage.setItem(curr_topic+"comments", savedcomments);
+}
+
+function dynamic_add(commentlist, i) {
+	var node = document.createElement("LI");
+	var textnode = document.createTextNode(comments_array[i]["userID"]);
+	
+	var userdiv = document.createElement('div');
+	userdiv.className = 'usernameText';
+	userdiv.appendChild(textnode);
+
+	var div = document.createElement('div');
+	div.className = 'commentText';
+	var p = document.createElement('p');
+	var commentnode = document.createTextNode(comments_array[i]["comment"])
+	p.appendChild(commentnode);
+	div.appendChild(p);
+	var span = document.createElement('span');
+	span.className = "date sub-text";
+	var datenode = document.createTextNode(comments_array[i]["datetime"]);
+	span.appendChild(datenode);
+	div.appendChild(span);
+
+	var likediv = document.createElement('div');
+	likediv.className = 'reactWrapper';
+
+	var thumbsup = document.createElement('input');
+	thumbsup.style.width = "20px";
+	thumbsup.style.height = "20px"
+	thumbsup.type = "image";
+	thumbsup.src = "images/thumbsup.jpg";
+	thumbsup.id = commentnode.nodeValue+";|,,likebutton";
+	thumbsup.setAttribute("onclick","increase_like(this)");
+
+	var thumbsup_number = document.createElement('div');
+	var likes = document.createTextNode(parseInt(comments_array[i]["likes"]));
+	thumbsup_number.appendChild(likes);
+	thumbsup_number.style.fontSize = "8px";
+	thumbsup_number.style.paddingBottom = "8px";
+	thumbsup_number.style.width = "20px";
+	thumbsup_number.style.textAlign = "center";
+	thumbsup_number.id = commentnode.nodeValue + ";|,,like";
+
+	likediv.appendChild(thumbsup);
+	likediv.appendChild(thumbsup_number);
+
+	var dislikediv = document.createElement('div');
+	dislikediv.className = 'reactWrapper';
+
+	var thumbsdown = document.createElement('input');
+	thumbsdown.style.width = "20px";
+	thumbsdown.style.height= "20px";
+	thumbsdown.type = "image";
+	thumbsdown.src = "images/thumbsdown.jpg";
+	thumbsdown.id = commentnode.nodeValue+";|,,dislikebutton";
+	thumbsdown.setAttribute("onclick","increase_dislike(this)");
+
+	var thumbsdown_number = document.createElement('div');
+	var dislikes = document.createTextNode(parseInt(comments_array[i]["dislikes"]));
+	thumbsdown_number.appendChild(dislikes);
+	thumbsdown_number.style.fontSize = "8px";
+	thumbsdown_number.style.paddingBottom = "8px";
+	thumbsdown_number.style.width = "20px";
+	thumbsdown_number.style.textAlign = "center";
+	thumbsdown_number.id = commentnode.nodeValue + ";|,,dislike";
+
+	dislikediv.appendChild(thumbsdown);
+	dislikediv.appendChild(thumbsdown_number);
+
+	node.appendChild(dislikediv);
+	node.appendChild(likediv);
+	node.appendChild(userdiv);
+
+	node.appendChild(div);
+
+	commentlist.append(node);
+}
+
 function initialize_comments() {
 	var commentlist = document.getElementById("commentListID");
-	for (i = 0; i < comments_array.length; i++) {
-		var node = document.createElement("LI");
-		var textnode = document.createTextNode(comments_array[i]["userID"]);
-		node.appendChild(textnode);
-
-		var div = document.createElement('div');
-		div.className = 'commentText';
-		var p = document.createElement('p');
-		var commentnode = document.createTextNode(comments_array[i]["comment"])
-		p.appendChild(commentnode);
-		div.appendChild(p);
-		var span = document.createElement('span');
-		span.className = "date sub-text";
-		var datenode = document.createTextNode(comments_array[i]["datetime"]);
-		span.appendChild(datenode);
-		div.appendChild(span);
-
-		node.appendChild(div);
-
-		commentlist.append(node);
+	for (var i = 0; i < comments_array.length; i++) {
+		dynamic_add(commentlist, i);
 	}
 }
 
@@ -108,10 +223,8 @@ function add_comment() {
 		commentdate = timeformat[0] + " " + timeformat[1] + " " + timeformat[2] + " " + timeformat[3] + " " + timeformat[4];
 
 		var newcomment_dict = {userID: username, comment: commentInput, datetime: commentdate, likes: 0, dislikes: 0};
-		comments_array.push(newcomment_dict);
+		comments_array.splice(0, 0,newcomment_dict);
 		append_comment(comments_array.length-1);
-		savedcomments = JSON.stringify(comments_array);
-		localStorage.setItem(curr_topic+"comments", savedcomments);
 		return true;
 	}
 	else {
@@ -124,27 +237,9 @@ function add_comment() {
 }
 
 function append_comment(index) {
-	var i = index;
 	var commentlist = document.getElementById("commentListID");
-	var node = document.createElement("LI");
-	var textnode = document.createTextNode(comments_array[i]["userID"]);
-	node.appendChild(textnode);
-
-	var div = document.createElement('div');
-	div.className = 'commentText';
-	var p = document.createElement('p');
-	var commentnode = document.createTextNode(comments_array[i]["comment"])
-	p.appendChild(commentnode);
-	div.appendChild(p);
-	var span = document.createElement('span');
-	span.className = "date sub-text";
-	var datenode = document.createTextNode(comments_array[i]["datetime"]);
-	span.appendChild(datenode);
-	div.appendChild(span);
-
-	node.appendChild(div);
-
-	commentlist.append(node);
+	dynamic_add(commentlist, index);
+	sort_comments();
 }
 
 initialize_comments();
